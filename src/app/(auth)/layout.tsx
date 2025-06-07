f072c5c6370dcafe6b72e89212ffbf4462f0e0d6
Meghraj Jare <meghrajjare77@gmail.com>
2025-06-07 21:35:58 +0530

    feat: Authentication

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,26 @@
+import { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
+
+export const metadata: Metadata = {
+  title: "Auth | LegalMind - Join the future of legal advice",
+  description:
+    "Sign up or log in to access AI-powered legal insights and personalized guidance.",
+};
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className}`}>{children}</body>
+    </html>
+  );
+}
