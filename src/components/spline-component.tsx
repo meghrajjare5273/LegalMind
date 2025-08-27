47054780546c3f91ad664c7285ad522963c52587
Meghraj Jare <meghrajjare77@gmail.com>
2025-08-27 10:32:02 +0530

    dash: Adding a spline component

---
+++ src/components/spline-component.tsx
@@ -0,0 +1,9 @@
+import Spline from "@splinetool/react-spline/next";
+
+export default function SplineScene() {
+  return (
+    <main>
+      <Spline scene="https://prod.spline.design/TiEcE1i3VzgzHdPK/scene.splinecode" />
+    </main>
+  );
+}
