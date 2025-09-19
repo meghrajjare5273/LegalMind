7fa34e114647e23894ee687e0c69bbdc8fa0fe28
Meghraj Jare <meghrajjare77@gmail.com>
2025-09-19 21:48:48 +0530

    chat: fixing light mode colors

--- src/app/(protected)/services/chat/layout.tsx
+++ src/app/(protected)/services/chat/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import dynamic from "next/dynamic";
 import { ChatSidebar } from "@/components/protected/chat/chat-sidebar";
 import { Inter } from "next/font/google";
+import ShadersBackground from "@/components/ui/liquid-metal";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
@@ -25,8 +26,8 @@ export default function ChatLayout({
     <div
       className={`${inter.variable} font-sans  text-neutral-950 dark:text-neutral-100 dark:bg-neutral-950 h-dvh w-dvw flex relative`}
     >
-      <div className="absolute inset-0 flex items-center top-25 left-10 justify-center z-0 backdrop-blur-3xl filter">
-        <RotatingEarth />
+      <div className="absolute inset-0 flex items-center justify-center z-0 backdrop-blur-3xl filter">
+        <ShadersBackground />
       </div>
 
       <div className="relative z-10 flex w-full  text-neutral-950 dark:text-neutral-200">
