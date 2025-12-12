ad8273cc6819addeaa32b880edd0d745bc5e7a06
Meghraj Jare <meghrajjare77@gmail.com>
2025-12-12 23:07:46 +0530

    refurb: fixing everything

--- next.config.ts
+++ next.config.ts
@@ -2,26 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    optimizeCss: true,
-    staleTimes: { dynamic: 30, static: 300 },
-  },
-
-  compress: true,
-
-  async headers() {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "private, s-maxage=60, stale-while-revalidate=120",
-          },
-        ],
-      },
-    ];
-  },
+  reactCompiler: true,
 };
 
 export default nextConfig;
